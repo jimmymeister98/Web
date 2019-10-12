d79c3c9e0bf274720a1fc4137caa0ce68f4932e0
Jimmy Neitzert <jimmy.neitzert@stud.hn.de>
2019-10-12 12:28:06 +0200

    Testgerüst, CherryPy server mit HTML Frontend

---
+++ ServerBackend.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return open ('ServerFrontend.html')
+
+    @cherrypy.expose
+    def test(self, length = 8):   #Wenn link "localhost/test?length=x" angegeben wird, wird ein random string der länge x erstellt
+        return ''.join(random.sample(string.hexdigits, int(length)))
+
+    @cherrypy.expose
+    def display(self):
+         return cherrypy.session['mystring']
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True
+        }
+    }
+    cherrypy.quickstart(StringGenerator(), '/', conf)
